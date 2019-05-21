dc3f6c15bc1aa8f57f747fd1eb4bfb4d87ffcbce
Zain Albastaki <zainalbastaki@Netsellss-MBP.localdomain>
2019-05-21 13:12:08 +0100

    Commit src change

---
+++ packages/eslint-config/rules/vue/component-name-in-template-casing/rule.js
@@ -0,0 +1,10 @@
+const _THROW = require('../../../modules/throwables');
+
+module.exports = {
+    rules: {
+        'vue/component-name-in-template-casing': [ _THROW.WARNING, 'kebab-case', {
+            'registeredComponentsOnly': true,
+            'ignores': [],
+        }],
+    },
+};
