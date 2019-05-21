aa48068b6f86cfc06af66c47d2544ca58e14d5b5
Zain Albastaki <zainalbastaki@Netsellss-MBP.localdomain>
2019-05-21 11:50:54 +0100

    Remove dot from extensions option in rule

--- packages/eslint-config/rules/vue/match-component-file-name/rule.js
+++ packages/eslint-config/rules/vue/match-component-file-name/rule.js
@@ -3,7 +3,7 @@ const _THROW = require('../../../modules/throwables');
 module.exports = {
     rules: {
         'vue/match-component-file-name': [_THROW.WARNING, {
-            'extensions': ['.jsx', '.js', '.vue'],
+            'extensions': ['jsx', 'js', 'vue'],
             'shouldMatchCase': false,
         }],
     },
