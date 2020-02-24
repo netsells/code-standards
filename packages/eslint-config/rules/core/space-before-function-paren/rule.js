259abb9f414d9d8317729ee7a7d6b50240b1ce76
Sam Turrell <sam.turrell@netsells.co.uk>
2020-02-24 16:34:05 +0000

    Test failing test

--- packages/eslint-config/rules/core/space-before-function-paren/rule.js
+++ packages/eslint-config/rules/core/space-before-function-paren/rule.js
@@ -1,16 +1,6 @@
 const _THROW = require('../../../modules/throwables');
 
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 8,
-        sourceType: 'module',
-        parser: 'babel-eslint',
-    },
-    env: {
-        es6: true,
-        node: true,
-        jquery: true,
-    },
     rules: {
         'space-before-function-paren': [_THROW.WARNING, {
             anonymous: 'never',
