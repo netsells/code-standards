7e58375132d0176cc78e858637326f85985aeb2b
Rowan Collins <rowan.collins@netsells.co.uk>
2020-02-28 15:38:01 +0000

    add warning rule vue/html-closing-bracket-spacing

--- packages/eslint-config/rules/vue/html-closing-bracket-spacing/rule.js
+++ packages/eslint-config/rules/vue/html-closing-bracket-spacing/rule.js
@@ -3,9 +3,9 @@ const _THROW = require('../../../modules/throwables');
 module.exports = {
     rules: {
         'vue/html-closing-bracket-spacing': [_THROW.WARNING, {
-            "startTag": "never",
-            "endTag": "never",
-            "selfClosingTag": "always"
+            'startTag': 'never',
+            'endTag': 'never',
+            'selfClosingTag': 'always',
         }],
     },
 };
