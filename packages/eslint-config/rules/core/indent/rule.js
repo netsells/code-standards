f6625f6e35a6743f7bfef219d81684733c993c69
Rowan Collins <rowan.collins@netsells.co.uk>
2020-03-03 16:05:53 +0000

    remove yet another string key

--- packages/eslint-config/rules/core/indent/rule.js
+++ packages/eslint-config/rules/core/indent/rule.js
@@ -2,7 +2,7 @@ const _THROW = require('../../../modules/throwables');
 
 module.exports = {
     rules: {
-        'indent': [_THROW.WARNING, 4],
+        indent: [_THROW.WARNING, 4],
     },
 
     overrides: [
