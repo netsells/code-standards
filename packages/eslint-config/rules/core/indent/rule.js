b123dcaea9213eb8d04f2470d69f0edb3098b2a8
Rowan Collins <rowan.collins@netsells.co.uk>
2020-03-03 15:57:52 +0000

    add rule for indent

---
+++ packages/eslint-config/rules/core/indent/rule.js
@@ -0,0 +1,16 @@
+const _THROW = require('../../../modules/throwables');
+
+module.exports = {
+    rules: {
+        'indent': [_THROW.WARNING, 4],
+    },
+
+    'overrides': [
+        {
+            'files': ['*.vue'],
+            'rules': {
+                'indent': 'off',
+            },
+        },
+    ],
+};
