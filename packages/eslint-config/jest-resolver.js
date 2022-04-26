d3774f6f5a2eeedd94930211299c8a3df6a415f4
Sam Turrell <sam.turrell@netsells.co.uk>
2022-04-26 09:07:39 +0100

    dependabot/npm_and_yarn/packages/eslint-config/jest-28.0.0 - 🔧 Fixed tests

--- packages/eslint-config/jest-resolver.js
+++ packages/eslint-config/jest-resolver.js
@@ -1,6 +1,12 @@
+const { resolve } = require('path');
+
 module.exports = function (request, options) {
     if (request.includes('use-at-your-own-risk')) {
-        request = 'eslint/lib/unsupported-api';
+        request = resolve(__dirname, 'node_modules/eslint/lib/unsupported-api.js');
+    }
+
+    if (request.includes('eslint/lib/cli-engine')) {
+        request = resolve(__dirname, 'node_modules/eslint/lib/cli-engine/index.js');
     }
 
     return options.defaultResolver(request, options);
