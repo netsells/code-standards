56ebde19bbfa06c956a5c932a5af96a937913caf
Becca Anderton <rebecca.anderton@netsells.co.uk>
2022-05-12 09:30:05 +0100

    Adds the arrow-parens rule

---
+++ packages/eslint-config/rules/core/arrow-parens/README.md
@@ -0,0 +1,1 @@
+Requires parenthesis around all arrow function arguments.
---
+++ packages/eslint-config/rules/core/arrow-parens/rule.js
@@ -0,0 +1,7 @@
+const _THROW = require('../../../modules/throwables');
+
+module.exports = {
+    rules: {
+        'arrow-parens': [_THROW.WARNING, 'always'],
+    },
+};
