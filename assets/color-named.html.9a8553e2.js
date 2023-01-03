903b7bb30910dbc8df8d6736920389bd31aa5fe3
samturrell <sam@samturrell.co.uk>
2023-01-03 10:07:22 +0000

    Deploying to gh-pages from @ netsells/code-standards@81106717ca8b08c40060ccc5daea8180683919ed 🚀

---
+++ assets/color-named.html.9a8553e2.js
@@ -0,0 +1,15 @@
+import{_ as i,r as e,o as r,c as u,a as n,d as a,w as t,b as s}from"./app.a8fb1227.js";const d={},p={id:"color-named",tabindex:"-1"},m=n("a",{class:"header-anchor",href:"#color-named","aria-hidden":"true"},"#",-1),_=n("p",null,"Colors must never be named.",-1),k=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),s(" Examples")],-1),h=n("div",{class:"language-css line-numbers-mode","data-ext":"css"},[n("pre",{class:"language-css"},[n("code",null,[n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(` 
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" #000000"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" $blue"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),b=n("div",{class:"language-css line-numbers-mode","data-ext":"css"},[n("pre",{class:"language-css"},[n("code",null,[n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(` 
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" black"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" white"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function v(g,x){const c=e("RouterLink"),o=e("badge"),l=e("code-highlight");return r(),u("div",null,[n("p",null,[a(c,{to:"/frontend/linting/stylelint/"},{default:t(()=>[s("← Back")]),_:1})]),n("h1",p,[m,s(" color-named "),a(o,{text:"warn",type:"warn",vertical:"middle"})]),_,k,a(l,null,{correct:t(()=>[h]),incorrect:t(()=>[b]),_:1})])}const y=i(d,[["render",v],["__file","color-named.html.vue"]]);export{y as default};
---
+++ assets/jsdoc-require-description.html.50846c25.js
@@ -0,0 +1,14 @@
+import{_ as l,r as s,o as r,c as d,a as n,d as a,w as t,b as e}from"./app.a8fb1227.js";const u={},p={id:"jsdoc-require-description",tabindex:"-1"},m=n("a",{class:"header-anchor",href:"#jsdoc-require-description","aria-hidden":"true"},"#",-1),_=n("p",null,"Requires that all functions have a description.",-1),h=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),e(" Examples")],-1),v=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},`/**
+ * Has an implicit description
+ */`),e(`
+`),n("span",{class:"token keyword"},"function"),e(),n("span",{class:"token function"},"quux"),e(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),e(),n("span",{class:"token punctuation"},"{"),e(`
+
+`),n("span",{class:"token punctuation"},"}"),e(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),b=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},`/**
+ *
+ */`),e(`
+`),n("span",{class:"token keyword"},"function"),e(),n("span",{class:"token function"},"quux"),e(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),e(),n("span",{class:"token punctuation"},"{"),e(`
+
+`),n("span",{class:"token punctuation"},"}"),e(`
+`),n("span",{class:"token comment"},"// Message: Missing JSDoc block description."),e(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function k(g,f){const c=s("RouterLink"),i=s("badge"),o=s("code-highlight");return r(),d("div",null,[n("p",null,[a(c,{to:"/frontend/linting/eslint/"},{default:t(()=>[e("← Back")]),_:1})]),n("h1",p,[m,e(" jsdoc/require-description "),a(i,{text:"warning",type:"warning",vertical:"middle"})]),_,h,a(o,null,{correct:t(()=>[v]),incorrect:t(()=>[b]),_:1})])}const j=l(u,[["render",k],["__file","jsdoc-require-description.html.vue"]]);export{j as default};
