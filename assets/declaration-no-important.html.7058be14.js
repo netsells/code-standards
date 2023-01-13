3dde2fe1f9f21e6ad585d03d8c527abbc460248b
samturrell <sam@samturrell.co.uk>
2023-01-13 11:15:39 +0000

    Deploying to gh-pages from @ netsells/code-standards@e43b7bd66c0a6f749749abeeb39f8162a0394025 🚀

---
+++ assets/declaration-no-important.html.7058be14.js
@@ -0,0 +1,15 @@
+import{_ as i,r as a,o as r,c as p,a as n,d as e,w as t,b as s}from"./app.a5153868.js";const u={},d={id:"declaration-no-important",tabindex:"-1"},m=n("a",{class:"header-anchor",href:"#declaration-no-important","aria-hidden":"true"},"#",-1),_=n("p",null,"Disallow !important within declarations.",-1),k=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),s(" Examples")],-1),h=n("div",{class:"language-css line-numbers-mode","data-ext":"css"},[n("pre",{class:"language-css"},[n("code",null,[n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" #ffffff"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),b=n("div",{class:"language-css line-numbers-mode","data-ext":"css"},[n("pre",{class:"language-css"},[n("code",null,[n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" #FFFFFF "),n("span",{class:"token important"},"!important"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" #FFFFFF"),n("span",{class:"token important"},"!important"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"color"),n("span",{class:"token punctuation"},":"),s(" #FFFFFF ! important"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function v(F,f){const o=a("RouterLink"),c=a("badge"),l=a("code-highlight");return r(),p("div",null,[n("p",null,[e(o,{to:"/frontend/linting/stylelint/"},{default:t(()=>[s("← Back")]),_:1})]),n("h1",d,[m,s(" declaration-no-important "),e(c,{text:"warn",type:"warn",vertical:"middle"})]),_,k,e(l,null,{correct:t(()=>[h]),incorrect:t(()=>[b]),_:1})])}const x=i(u,[["render",v],["__file","declaration-no-important.html.vue"]]);export{x as default};
---
+++ assets/index.html.a8528621.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-551bb614","path":"/backend/","title":"Backend","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1673608461000,"contributors":[{"name":"Sam Turrell","email":"sam.turrell@netsells.co.uk","commits":1}]},"filePathRelative":"backend/README.md"}');export{e as data};
