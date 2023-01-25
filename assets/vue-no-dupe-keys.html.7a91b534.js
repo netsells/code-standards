be42c32e9c4fb2899b74fcff94eea12fae342b3c
samturrell <sam@samturrell.co.uk>
2023-01-25 11:39:25 +0000

    Deploying to gh-pages from @ netsells/code-standards@03999aaff18c21b23f9def3f6ace658d36c89bc8 🚀

---
+++ assets/vue-no-dupe-keys.html.7a91b534.js
@@ -0,0 +1,24 @@
+import{_ as p,r as a,o as i,c as u,a as n,d as e,w as t,b as s}from"./app.2be90a9a.js";const r={},k={id:"vue-no-dupe-keys",tabindex:"-1"},d=n("a",{class:"header-anchor",href:"#vue-no-dupe-keys","aria-hidden":"true"},"#",-1),m=n("p",null,"Disallow duplicate key names to avoid overwriting",-1),v=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),s(" Examples")],-1),_=n("div",{class:"language-vue line-numbers-mode","data-ext":"vue"},[n("pre",{class:"language-vue"},[n("code",null,[n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"<"),s("script")]),n("span",{class:"token punctuation"},">")]),n("span",{class:"token script"},[n("span",{class:"token language-javascript"},[s(`
+    `),n("span",{class:"token keyword"},"export"),s(),n("span",{class:"token keyword"},"default"),s(),n("span",{class:"token punctuation"},"{"),s(`
+        `),n("span",{class:"token literal-property property"},"props"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token literal-property property"},"foo"),n("span",{class:"token operator"},":"),s(" String"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+
+        `),n("span",{class:"token literal-property property"},"computed"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token literal-property property"},"foo"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+                `),n("span",{class:"token function"},"get"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"}"),s(`
+            `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+
+        `),n("span",{class:"token function"},"data"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token keyword"},"return"),s(),n("span",{class:"token punctuation"},"{"),s(`
+                `),n("span",{class:"token literal-property property"},"foo"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token keyword"},"null"),n("span",{class:"token punctuation"},","),s(`
+            `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},";"),s(`
+        `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+
+        `),n("span",{class:"token literal-property property"},"methods"),n("span",{class:"token operator"},":"),s(),n("span",{class:"token punctuation"},"{"),s(`
+            `),n("span",{class:"token function"},"foo"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+        `),n("span",{class:"token punctuation"},"}"),n("span",{class:"token punctuation"},","),s(`
+    `),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("span",{class:"token tag"},[n("span",{class:"token tag"},[n("span",{class:"token punctuation"},"</"),s("script")]),n("span",{class:"token punctuation"},">")]),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function b(h,y){const o=a("RouterLink"),c=a("badge"),l=a("code-highlight");return i(),u("div",null,[n("p",null,[e(o,{to:"/frontend/linting/eslint/"},{default:t(()=>[s("← Back")]),_:1})]),n("h1",k,[d,s(" vue/no-dupe-keys "),e(c,{text:"warning",type:"warning",vertical:"middle"})]),m,v,e(l,null,{incorrect:t(()=>[_]),_:1})])}const f=p(r,[["render",b],["__file","vue-no-dupe-keys.html.vue"]]);export{f as default};
