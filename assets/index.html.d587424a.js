a3ad75c885d6400e6ea601c746a3c270680c4aed
samturrell <sam@samturrell.co.uk>
2023-02-08 17:13:29 +0000

    Deploying to gh-pages from @ netsells/code-standards@90dfc049d5c8408fffd820d1bdd03ba56302a5e4 🚀

--- assets/index.html.d587424a.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-59c1b58c","path":"/flutter/","title":"Legacy Flutter Projects","lang":"en-US","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1675876314000,"contributors":[{"name":"Sam Turrell","email":"sam.turrell@netsells.co.uk","commits":1}]},"filePathRelative":"flutter/README.md"}');export{e as data};
---
+++ assets/yoda.html.d12fc2d4.js
@@ -0,0 +1,27 @@
+import{_ as i,r as a,o as p,c as u,a as n,d as e,w as t,b as s}from"./app.bd881477.js";const r={},d={id:"yoda",tabindex:"-1"},k=n("a",{class:"header-anchor",href:"#yoda","aria-hidden":"true"},"#",-1),m=n("p",null,"Discourage code typed like yoda would speak",-1),b=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),s(" Examples")],-1),v=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token number"},"5"),s(),n("span",{class:"token operator"},"&"),s(" value"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),s("value "),n("span",{class:"token operator"},"==="),s(),n("span",{class:"token string"},"'red'"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),s("x "),n("span",{class:"token operator"},"<"),s(),n("span",{class:"token operator"},"-"),n("span",{class:"token number"},"1"),s(),n("span",{class:"token operator"},"||"),s(),n("span",{class:"token number"},"1"),s(),n("span",{class:"token operator"},"<"),s(" x"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),_=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token string"},"'red'"),s(),n("span",{class:"token operator"},"==="),s(" color"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token boolean"},"true"),s(),n("span",{class:"token operator"},"=="),s(" flag"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token number"},"5"),s(),n("span",{class:"token operator"},">"),s(" count"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token keyword"},"if"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token operator"},"-"),n("span",{class:"token number"},"1"),s(),n("span",{class:"token operator"},"<"),s(" str"),n("span",{class:"token punctuation"},"."),n("span",{class:"token function"},"indexOf"),n("span",{class:"token punctuation"},"("),s("substr"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token comment"},"// ..."),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function h(f,g){const o=a("RouterLink"),c=a("badge"),l=a("code-highlight");return p(),u("div",null,[n("p",null,[e(o,{to:"/frontend/linting/eslint/"},{default:t(()=>[s("← Back")]),_:1})]),n("h1",d,[k,s(" yoda "),e(c,{text:"warning",type:"warning",vertical:"middle"})]),m,b,e(l,null,{correct:t(()=>[v]),incorrect:t(()=>[_]),_:1})])}const y=i(r,[["render",h],["__file","yoda.html.vue"]]);export{y as default};
