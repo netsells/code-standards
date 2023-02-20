1f41e42a7f382ec2d96e9f7edf8f9a5dff46fe8d
samturrell <sam@samturrell.co.uk>
2023-02-20 10:40:54 +0000

    Deploying to gh-pages from @ netsells/code-standards@efa3aa101dc528e63664147a61327bee9ebf61db 🚀

---
+++ assets/jsdoc-check-types.html.6ad6139b.js
@@ -0,0 +1,97 @@
+import{_ as p,r as a,o as u,c as i,a as n,d as e,w as t,b as s,e as r}from"./app.810b580e.js";const k={},m={id:"jsdoc-check-types",tabindex:"-1"},d=n("a",{class:"header-anchor",href:"#jsdoc-check-types","aria-hidden":"true"},"#",-1),b=r('<p>Reports invalid types.</p><p>By default, ensures that the casing of native types is the same as in this list:</p><ul><li><code>undefined</code></li><li><code>null</code></li><li><code>boolean</code></li><li><code>number</code></li><li><code>bigint</code></li><li><code>string</code></li><li><code>symbol</code></li><li><code>object</code></li><li><code>Array</code></li><li><code>Function</code></li><li><code>Date</code></li><li><code>RegExp</code></li></ul><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2>',4),v=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("number"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Bar"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"bar"),s(`
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token operator"},"*"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"baz"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux1"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},[s("foo"),n("span",{class:"token punctuation"},","),s(" bar"),n("span",{class:"token punctuation"},","),s(" baz")]),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@arg"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("number"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ * `),n("span",{class:"token keyword"},"@arg"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Bar"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"bar"),s(`
+ * `),n("span",{class:"token keyword"},"@arg"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token operator"},"*"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"baz"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux2"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},[s("foo"),n("span",{class:"token punctuation"},","),s(" bar"),n("span",{class:"token punctuation"},","),s(" baz")]),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"("),s("number"),n("span",{class:"token operator"},"|"),s("string"),n("span",{class:"token operator"},"|"),s("boolean"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},"="),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux3"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},[s("foo"),n("span",{class:"token punctuation"},","),s(" bar"),n("span",{class:"token punctuation"},","),s(" baz")]),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token keyword"},"typeof"),s(" bar"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"qux1"),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token keyword"},"import"),n("span",{class:"token punctuation"},"("),n("span",{class:"token string"},"'./foo'"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},"."),s("bar"),n("span",{class:"token punctuation"},"."),s("baz"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"qux2"),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"("),s("x"),n("span",{class:"token operator"},":"),s(" number"),n("span",{class:"token punctuation"},","),s(" y"),n("span",{class:"token operator"},":"),s(" string"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token operator"},"=>"),s(" string"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"qux3"),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token operator"},"=>"),s(" string"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"qux4"),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),f=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Number"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux1"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},'// Message: Invalid JSDoc @param "foo" type "Number"; prefer: "number".'),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@arg"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Number"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux2"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},'// Message: Invalid JSDoc @arg "foo" type "Number"; prefer: "number".'),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@returns"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Number"),n("span",{class:"token punctuation"},"}")]),s(` foo
+ * `),n("span",{class:"token keyword"},"@throws"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Number"),n("span",{class:"token punctuation"},"}")]),s(` foo
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux3"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},'// Message: Invalid JSDoc @returns type "Number"; prefer: "number".'),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"("),s("Number"),n("span",{class:"token operator"},"|"),s("string"),n("span",{class:"token operator"},"|"),s("Boolean"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},"="),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux4"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},[s("foo"),n("span",{class:"token punctuation"},","),s(" bar"),n("span",{class:"token punctuation"},","),s(" baz")]),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},'// Message: Invalid JSDoc @param "foo" type "Number"; prefer: "number".'),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("Array"),n("span",{class:"token punctuation"},"."),n("span",{class:"token punctuation"},"<"),s("Number"),n("span",{class:"token operator"},"|"),s("String"),n("span",{class:"token punctuation"},">"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux5"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},[s("foo"),n("span",{class:"token punctuation"},","),s(" bar"),n("span",{class:"token punctuation"},","),s(" baz")]),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},'// Message: Invalid JSDoc @param "foo" type "Number"; prefer: "number".'),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@param"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),n("span",{class:"token punctuation"},"("),s("Number"),n("span",{class:"token operator"},"|"),s("String"),n("span",{class:"token punctuation"},")"),n("span",{class:"token punctuation"},"["),n("span",{class:"token punctuation"},"]"),n("span",{class:"token punctuation"},"}")]),s(),n("span",{class:"token parameter"},"foo"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux6"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},[s("foo"),n("span",{class:"token punctuation"},","),s(" bar"),n("span",{class:"token punctuation"},","),s(" baz")]),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},'// Message: Invalid JSDoc @param "foo" type "Number"; prefer: "number".'),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function y(g,h){const o=a("RouterLink"),c=a("badge"),l=a("code-highlight");return u(),i("div",null,[n("p",null,[e(o,{to:"/frontend/linting/eslint/"},{default:t(()=>[s("← Back")]),_:1})]),n("h1",m,[d,s(" jsdoc/check-types "),e(c,{text:"warning",type:"warning",vertical:"middle"})]),b,e(l,null,{correct:t(()=>[v]),incorrect:t(()=>[f]),_:1})])}const _=p(k,[["render",y],["__file","jsdoc-check-types.html.vue"]]);export{_ as default};
---
+++ assets/shorthand-property-no-redundant-values.html.edf0f893.js
@@ -0,0 +1,31 @@
+import{_ as p,r as a,o as i,c as r,a as n,d as e,w as t,b as s}from"./app.810b580e.js";const u={},d={id:"shorthand-property-no-redundant-values",tabindex:"-1"},m=n("a",{class:"header-anchor",href:"#shorthand-property-no-redundant-values","aria-hidden":"true"},"#",-1),k=n("p",null,"Disallow redundant values in shorthand properties.",-1),v=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),s(" Examples")],-1),b=n("div",{class:"language-css line-numbers-mode","data-ext":"css"},[n("pre",{class:"language-css"},[n("code",null,[n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"margin"),n("span",{class:"token punctuation"},":"),s(" 1px"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"margin"),n("span",{class:"token punctuation"},":"),s(" 1px 1px 1px 2px"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"padding"),n("span",{class:"token punctuation"},":"),s(" 1px 1em 1pt 1pc"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token property"},"border-radius"),n("span",{class:"token punctuation"},":"),s(" 10px / 5px"),n("span",{class:"token punctuation"},";"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),h=n("div",{class:"language-css line-numbers-mode","data-ext":"css"},[n("pre",{class:"language-css"},[n("code",null,[n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(` 
+    `),n("span",{class:"token property"},"margin"),n("span",{class:"token punctuation"},":"),s(" 1px 1px"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(` 
+    `),n("span",{class:"token property"},"margin"),n("span",{class:"token punctuation"},":"),s(" 1px 1px 1px 1px"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(` 
+    `),n("span",{class:"token property"},"padding"),n("span",{class:"token punctuation"},":"),s(" 1px 2px 1px"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token selector"},"a"),s(),n("span",{class:"token punctuation"},"{"),s(` 
+    `),n("span",{class:"token property"},"border-radius"),n("span",{class:"token punctuation"},":"),s(" 1px 2px 1px 2px"),n("span",{class:"token punctuation"},";"),s(` 
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function x(_,g){const c=a("RouterLink"),l=a("badge"),o=a("code-highlight");return i(),r("div",null,[n("p",null,[e(c,{to:"/frontend/linting/stylelint/"},{default:t(()=>[s("← Back")]),_:1})]),n("h1",d,[m,s(" shorthand-property-no-redundant-values "),e(l,{text:"warn",type:"warn",vertical:"middle"})]),k,v,e(o,null,{correct:t(()=>[b]),incorrect:t(()=>[h]),_:1})])}const f=p(u,[["render",x],["__file","shorthand-property-no-redundant-values.html.vue"]]);export{f as default};
