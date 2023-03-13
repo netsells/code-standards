e05ebd9dd449e3d8607eaa56b7847470e650b6de
samturrell <sam@samturrell.co.uk>
2023-03-13 11:22:15 +0000

    Deploying to gh-pages from @ netsells/code-standards@67efc4a9fdfac4bf872cdf07cd4548165ff54b02 🚀

---
+++ assets/jsdoc-implements-on-classes.html.4bfd698a.js
@@ -0,0 +1,54 @@
+import{_ as i,r as e,o as u,c as p,a as n,d as a,w as c,b as s,e as d}from"./app.5403719a.js";const m={},r={id:"jsdoc-implements-on-classes",tabindex:"-1"},k=n("a",{class:"header-anchor",href:"#jsdoc-implements-on-classes","aria-hidden":"true"},"#",-1),b=d('<p>Reports an issue with any non-constructor function using <code>@implements</code>.</p><p>Constructor functions, whether marked with <code>@class</code>, <code>@constructs</code>, or being an ES6 class constructor, will not be flagged.</p><p>To indicate that a function follows another function&#39;s signature, one might instead use <code>@type</code> to indicate the <code>@function</code> or <code>@callback</code> to which the function is adhering.</p><h2 id="examples" tabindex="-1"><a class="header-anchor" href="#examples" aria-hidden="true">#</a> Examples</h2>',4),v=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@implements"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("SomeClass"),n("span",{class:"token punctuation"},"}")]),s(`
+ * `),n("span",{class:"token keyword"},"@class"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux1"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@implements"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("SomeClass"),n("span",{class:"token punctuation"},"}")]),s(`
+ * `),n("span",{class:"token keyword"},"@constructor"),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux2"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},`/**
+ *
+ */`),s(`
+`),n("span",{class:"token keyword"},"class"),s(),n("span",{class:"token class-name"},"quux3"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token doc-comment comment"},[s(`/**
+     * `),n("span",{class:"token keyword"},"@implements"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("SomeClass"),n("span",{class:"token punctuation"},"}")]),s(`
+     */`)]),s(`
+    `),n("span",{class:"token function"},"constructor"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+    `),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},`/**
+ *
+ */`),s(`
+`),n("span",{class:"token keyword"},"const"),s(" quux4 "),n("span",{class:"token operator"},"="),s(),n("span",{class:"token keyword"},"class"),s(),n("span",{class:"token punctuation"},"{"),s(`
+    `),n("span",{class:"token doc-comment comment"},[s(`/**
+     * `),n("span",{class:"token keyword"},"@implements"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("SomeClass"),n("span",{class:"token punctuation"},"}")]),s(`
+     */`)]),s(`
+    `),n("span",{class:"token function"},"constructor"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+    `),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token punctuation"},"}"),s(`
+
+`),n("span",{class:"token doc-comment comment"},`/**
+ *
+ */`),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux5"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),h=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},[s(`/**
+ * `),n("span",{class:"token keyword"},"@implements"),s(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),s("SomeClass"),n("span",{class:"token punctuation"},"}")]),s(`
+ */`)]),s(`
+`),n("span",{class:"token keyword"},"function"),s(),n("span",{class:"token function"},"quux"),s(),n("span",{class:"token punctuation"},"("),n("span",{class:"token punctuation"},")"),s(),n("span",{class:"token punctuation"},"{"),s(`
+
+`),n("span",{class:"token punctuation"},"}"),s(`
+`),n("span",{class:"token comment"},"// Message: @implements used on a non-constructor function"),s(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function _(f,g){const t=e("RouterLink"),o=e("badge"),l=e("code-highlight");return u(),p("div",null,[n("p",null,[a(t,{to:"/frontend/linting/eslint/"},{default:c(()=>[s("← Back")]),_:1})]),n("h1",r,[k,s(" jsdoc/implements-on-classes "),a(o,{text:"warning",type:"warning",vertical:"middle"})]),b,a(l,null,{correct:c(()=>[v]),incorrect:c(()=>[h]),_:1})])}const x=i(m,[["render",_],["__file","jsdoc-implements-on-classes.html.vue"]]);export{x as default};
---
+++ assets/jsdoc-require-param-type.html.4f623aa1.js
@@ -0,0 +1,21 @@
+import{_ as i,r as s,o as r,c as u,a as n,d as a,w as t,b as e}from"./app.5403719a.js";const p={},d={id:"jsdoc-require-param-type",tabindex:"-1"},m=n("a",{class:"header-anchor",href:"#jsdoc-require-param-type","aria-hidden":"true"},"#",-1),k=n("p",null,[e("Requires that each "),n("code",null,"@param"),e(" tag has a type value.")],-1),v=n("h2",{id:"examples",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#examples","aria-hidden":"true"},"#"),e(" Examples")],-1),_=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},`/**
+ *
+ */`),e(`
+`),n("span",{class:"token keyword"},"function"),e(),n("span",{class:"token function"},"quux1"),e(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),e(),n("span",{class:"token punctuation"},"{"),e(`
+
+`),n("span",{class:"token punctuation"},"}"),e(`
+
+`),n("span",{class:"token doc-comment comment"},[e(`/**
+ * `),n("span",{class:"token keyword"},"@param"),e(),n("span",{class:"token class-name"},[n("span",{class:"token punctuation"},"{"),e("number"),n("span",{class:"token punctuation"},"}")]),e(),n("span",{class:"token parameter"},"foo"),e(`
+ */`)]),e(`
+`),n("span",{class:"token keyword"},"function"),e(),n("span",{class:"token function"},"quux2"),e(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),e(),n("span",{class:"token punctuation"},"{"),e(`
+
+`),n("span",{class:"token punctuation"},"}"),e(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1),h=n("div",{class:"language-javascript line-numbers-mode","data-ext":"js"},[n("pre",{class:"language-javascript"},[n("code",null,[n("span",{class:"token doc-comment comment"},[e(`/**
+ * `),n("span",{class:"token keyword"},"@param"),e(),n("span",{class:"token parameter"},"foo"),e(`
+ */`)]),e(`
+`),n("span",{class:"token keyword"},"function"),e(),n("span",{class:"token function"},"quux"),e(),n("span",{class:"token punctuation"},"("),n("span",{class:"token parameter"},"foo"),n("span",{class:"token punctuation"},")"),e(),n("span",{class:"token punctuation"},"{"),e(`
+
+`),n("span",{class:"token punctuation"},"}"),e(`
+`),n("span",{class:"token comment"},'// Message: Missing JSDoc @param "foo" type.'),e(`
+`)])]),n("div",{class:"line-numbers","aria-hidden":"true"},[n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"}),n("div",{class:"line-number"})])],-1);function b(f,g){const c=s("RouterLink"),o=s("badge"),l=s("code-highlight");return r(),u("div",null,[n("p",null,[a(c,{to:"/frontend/linting/eslint/"},{default:t(()=>[e("← Back")]),_:1})]),n("h1",d,[m,e(" jsdoc/require-param-type "),a(o,{text:"warning",type:"warning",vertical:"middle"})]),k,v,a(l,null,{correct:t(()=>[_]),incorrect:t(()=>[h]),_:1})])}const y=i(p,[["render",b],["__file","jsdoc-require-param-type.html.vue"]]);export{y as default};
