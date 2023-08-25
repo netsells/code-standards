ffbbb21553cc9eb01b948ecfb013445aa77bec0e
samturrell <sam@samturrell.co.uk>
2023-08-25 09:17:32 +0000

    Deploying to gh-pages from @ netsells/code-standards@6b1838bb0b18d61354d44a91ef2ab312df7d47cd 🚀

--- assets/localization.html-5776ecad.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-22e0faa6","path":"/flutter/legacy/localization.html","title":"Localizing Flutter Apps","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1692608370000,"contributors":[{"name":"Sam Turrell","email":"sam.turrell@netsells.co.uk","commits":1}]},"filePathRelative":"flutter/legacy/localization.md"}');export{t as data};
