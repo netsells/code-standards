a7a9bf310f18ebe17fde09e8a540edc540c290d7
samturrell <sam@samturrell.co.uk>
2023-09-28 10:34:08 +0000

    Deploying to gh-pages from @ netsells/code-standards@ae4768dfa57a33238ca440291d8c5935988e92ac 🚀

---
+++ assets/layouts.html-27a9b7f1.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-e5d488aa","path":"/ios/layouts.html","title":"Layouts","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1695897179000,"contributors":[{"name":"Sam Turrell","email":"sam.turrell@netsells.co.uk","commits":1}]},"filePathRelative":"ios/layouts.md"}');export{t as data};
