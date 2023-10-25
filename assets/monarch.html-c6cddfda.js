bfaf6ec53a26ee9610b130b56a6d90252b21366e
samturrell <sam@samturrell.co.uk>
2023-10-25 12:40:40 +0000

    Deploying to gh-pages from @ netsells/code-standards@308d7937bdbef37f6b0f8de371bf67a8375e4253 🚀

---
+++ assets/monarch.html-c6cddfda.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-3a4b8043","path":"/flutter/monarch.html","title":"Monarch","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1698237585000,"contributors":[{"name":"Sam Turrell","email":"sam.turrell@netsells.co.uk","commits":1}]},"filePathRelative":"flutter/monarch.md"}');export{t as data};
