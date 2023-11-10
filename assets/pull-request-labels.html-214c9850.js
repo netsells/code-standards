cbb947ad09b389f6522c1fc9ba4fc417f7531d75
samturrell <sam@samturrell.co.uk>
2023-11-10 10:22:25 +0000

    Deploying to gh-pages from @ netsells/code-standards@04884167ca9bf24393bb5e44abcd3a49afece57b 🚀

---
+++ assets/pull-request-labels.html-214c9850.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-67bfbd0d","path":"/android/pull-request-labels.html","title":"Pull Request Labelling","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1699611686000,"contributors":[{"name":"Sam Turrell","email":"sam.turrell@netsells.co.uk","commits":1}]},"filePathRelative":"android/pull-request-labels.md"}');export{e as data};
